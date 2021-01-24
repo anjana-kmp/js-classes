1f7df5b79577a677ab3b0378db2cfd779897dbf1
anjana-kmp <71718802+anjana-kmp@users.noreply.github.com>
2021-01-24 13:25:09 +0530

    (a) correcting growth of plant from the bottom and (b) adding flowering on growth functionality.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const plant1=new Plant('rose',0,2,'./images/rose.jpg',[0,60,100,15]);
-const plant2=new Plant('orchid',0,1,'./images/orchid.png',[100,105,100,15]);
-const plant3=new Plant('tulsi',0,3,'./images/tulsi.webp',[200,30,100,15]);
+const plant1=new Plant('rose',0,2,'./images/cartoon-rose.png',[0,145,100,15]);
+const plant2=new Plant('orchid',0,1,'./images/cartoon-orchid.png',[105,140,100,15]);
+const plant3=new Plant('tulsi',0,3,'./images/cartoon-tulsi.png',[200,145,100,15]);
 
 
 // plant1.grow();
--- plant.js
+++ plant.js
@@ -2,38 +2,60 @@ class Plant{
     age = 0;
     name = '';
     growthFactor = 0;
-    image='';
+    image=null;
+    flowerImage=null
     rect={
       x:0,y:0,
       w:0,h:0
     };
-    constructor(name,age,growthFactor,imgPath,rect){
+    floweringAge=0
+    constructor(name,age,growthFactor,imgPath,rect,floweringAge){
       this.rect.x=rect[0]
       this.rect.y=rect[1]
       this.rect.w=rect[2]
       this.rect.h=rect[3]
-
+      this.floweringAge=floweringAge ||0
       this.name=name
       this.age=age
       this.growthFactor=growthFactor
 
       this.image=new Image();
       this.image.src=imgPath;
+      this.flowerImage=new Image();
+      
+      this.flowerImage.src=imgPath.replace('.png','-flowers.png')
+      
       this.image.onload=()=>{
         let canvas = document.querySelector('#plant-canvas');
         let ctx = canvas.getContext('2d');
         ctx.drawImage(this.image,this.rect.x,this.rect.y,this.rect.w,this.rect.h*this.age);
-       
+        
       }
+      this.init()
 
     }
-     grow(){
-       this.age+=this.growthFactor 
-       let canvas = document.querySelector('#plant-canvas');
-       let ctx = canvas.getContext('2d');
-      ctx.clearRect(this.rect.x,this.rect.y,this.rect.w,this.rect.h*this.age);
-      ctx.drawImage(this.image,this.rect.x,this.rect.y,this.rect.w,this.rect.h*this.age);
+    init(){
+      let btn=document.createElement('button')
+      btn.textContent=this.name.toUpperCase()
+      document.querySelector('.buttons').appendChild(btn)
+      btn.addEventListener('click',()=>{
+        this.grow()
 
+      })
+    }
+     grow(){
+       this.age += this.growthFactor 
+       let canvas = document.querySelector('#plant-canvas');
+       let ctx = canvas.getContext('2d')
+      let startY=(this.rect.h*this.age)
+      ctx.clearRect(this.rect.x,this.rect.y-startY,this.rect.w,this.rect.h*this.age);
+      console.log(this.rect.x,this.rect.y-startY,this.rect.w,this.rect.h*this.age);
+
+      ctx.drawImage(this.image,this.rect.x,this.rect.y-startY,this.rect.w,startY);
+      if(this.age*this.growthFactor>10){
+        ctx.drawImage(this.flowerImage,this.rect.x,this.rect.y-startY,this.rect.w,startY)
+      }
+      
     }
 
 }
